a84d7bea51c86488ea757b6f7d7905d349d9f151
Eason Lin <easonlin404@gmail.com>
2017-08-11 00:31:35 +0800

    feat(ksm): implement OAEPPDecrypt

--- ksm.go
+++ ksm.go
@@ -2,7 +2,11 @@ package ksm
 
 import (
 	"encoding/binary"
+	"errors"
+	_ "fmt"
 	"github.com/easonlin404/ksm/rsa"
+	"fmt"
+	"encoding/hex"
 )
 
 type SPCContainer struct {
@@ -15,8 +19,6 @@ type SPCContainer struct {
 	SPCPlayloadLength uint32
 }
 
-
-
 // This function will compute the content key context returned to client by the SKDServer library.
 //       incoming server playback context (SPC message)
 func GenCKC(playback []byte) error {
@@ -24,23 +26,43 @@ func GenCKC(playback []byte) error {
 	return nil
 }
 
-func ParseSPCContainer(playback []byte,reader rsa.Reader) (*SPCContainer, error) {
+func ParseSPCContainer(playback []byte, reader rsa.Reader) (*SPCContainer, error) {
+	fmt.Println("========================= Begin SPC Data ===============================")
+
 	spcContainer := &SPCContainer{}
 
 	spcContainer.Version = binary.BigEndian.Uint32(playback[0:4])
 	spcContainer.AesKeyIV = playback[8:24]
+	spck := playback[24:152]
+	spcContainer.EncryptedAesKey = spck
 
-	//TODO: get RSA private key
-	pem,err:=reader.ReadPem()
-	if err != nil {
-		return spcContainer, err
-	}
 
-	encryptedAesKey, err := rsa.Decrypt(pem, playback[24:152])
-	if err != nil {
-		return spcContainer, err
-	}
-	spcContainer.EncryptedAesKey = encryptedAesKey
+	//encryptedAesKey, err := rsa.Decrypt(pem, playback[24:152])
 
+	//fmt.Println("[SPCK]:" + hex.EncodeToString(spck))
+	//fmt.Println(len(hex.EncodeToString(spck)))
+	//pem, err := reader.ReadPem()
+	//if err != nil {
+	//	return spcContainer, err
+	//}
+	//encryptedAesKey, err := decryptSPCK(pem, spck) //TODO: not this
+
+
+
+
+
+	fmt.Println("SPC Encryption Key -")
+	fmt.Println(hex.EncodeToString(spck))
+	fmt.Println("SPC Encryption IV -")
+	fmt.Println(hex.EncodeToString(spcContainer.AesKeyIV))
+	fmt.Println("================ SPC TLLV List ================")
+	//TODO:
 	return spcContainer, nil
+}
+
+func decryptSPCK(pkPem, enSpck []byte) ([]byte, error) {
+	if len(enSpck) != 128 {
+		return nil, errors.New("Wrong [SPCK] length, must be 128")
+	}
+	return rsa.OAEPPDecrypt(pkPem, enSpck)
 }
--- ksm_test.go
+++ ksm_test.go
@@ -1,27 +1,27 @@
 package ksm
 
 import (
-	"encoding/hex"
-	"fmt"
 	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"os"
 	"testing"
 	"github.com/easonlin404/ksm/rsa"
+	"encoding/hex"
 )
 
 type spcContainerTest struct {
 	filePath string
 	size     int
 
-	SPCContainer
+	iv string
+	spec string
 }
 
 var spcContainerTests = []spcContainerTest{
 	{"testdata/spc1.bin", 2688,
-		SPCContainer{
-			AesKeyIV: []byte{93, 22, 68, 234, 236, 17, 249, 131, 20, 117, 65, 228, 110, 235, 39, 116},
-		}},
+	"5d1644eaec11f983147541e46eeb2774",
+		"926648b9861ec0471ba21758851c3dda31c93b1dd601aa4ead4415a20759aab9a6d89f551385856e73571729df2f1d46d25c13da2ad75d00fd3413ebd96ca47d02955c569f7fab40f1a7fb23414167a653eabdf1ad283df5e07e7cf4aa2fbac64f1d460fdf9a21eeb27a7f60727853a414c1c450c525e8dab6a3f13cfa57171a",
+	},
 }
 
 func TestGenCKC(t *testing.T) {
@@ -52,9 +52,9 @@ func TestParseSPCContainer(t *testing.T) {
 		spcContainer, err := ParseSPCContainer(spcMessage,FileReader)
 		assert.NoError(t, err)
 
-		assert.Equal(t, test.AesKeyIV, spcContainer.AesKeyIV)
+		assert.Equal(t, test.iv, hex.EncodeToString(spcContainer.AesKeyIV))
+		assert.Equal(t, test.spec, hex.EncodeToString(spcContainer.EncryptedAesKey))
 
-		fmt.Println(hex.EncodeToString(spcContainer.EncryptedAesKey))
 
 	}
 }
