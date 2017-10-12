ea03b46f05c0df1feb354db68dd92b0bf65b8d03
Eason Lin <easonlin404@gmail.com>
2017-10-12 10:29:17 +0800

    doc(ckc): enhance golint.

--- ckc.go
+++ ckc.go
@@ -5,7 +5,7 @@ import (
 	mathRand "math/rand"
 )
 
-// CKCPayload is a interface that fetch asset content key.
+// ContentKey is a interface that fetch asset content key and duration.
 type ContentKey interface {
 	FetchContentKey(assetId []byte) ([]byte, []byte, error)
 	FetchContentKeyDuration(assetId []byte) (*CkcContentKeyDurationBlock, error)
@@ -15,6 +15,7 @@ var (
 	_ ContentKey = RandomContentKey{}
 )
 
+// CKCPayload is a object that implements ContentKey interface.
 type RandomContentKey struct {
 }
 
@@ -26,6 +27,7 @@ func (RandomContentKey) FetchContentKey(assetId []byte) ([]byte, []byte, error) 
 	return key, iv, nil
 }
 
+// FetchContentKeyDuration returns CkcContentKeyDurationBlock for the given assetId.
 func (RandomContentKey) FetchContentKeyDuration(assetId []byte) (*CkcContentKeyDurationBlock, error) {
 
 	LeaseDuration := mathRand.Uint32()  // The duration of the lease, if any, in seconds.
